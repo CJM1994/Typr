10f801b4a41360ad92d7144e7ebddf11fc70269c
CJM1994 <connorjohnmullin@gmail.com>
2022-04-15 17:14:33 -0700

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
-router.get("/test", (req, res) => {
-
-});
-=======
 const { getUser } = require('../controllers/userRoutes');
 
 router.get("/user", getUser );
->>>>>>> 84bbdccd68e9a63df9b025aa7adfa53bc77162db
 
 module.exports = router;
