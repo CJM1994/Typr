b86e55084da06ed2fbed9ac5c06d95a13e497fac
trijaychan <trijaychan@gmail.com>
2022-04-16 17:39:23 -0700

    Add timer for typer

--- client/src/components/Main/Display.js
+++ client/src/components/Main/Display.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useKeyPress from "../../hooks/useKeyPress";
+import useTimer from "../../hooks/useTimer";
 
 import "./Display.scss";
 
@@ -9,14 +10,32 @@ import VirtualKeyboard from "./VirtualKeyboard";
 
 export default function Display() {
   // deconstructing objects
-  const { prompt, input, lengths, newPrompt, handleKeypress, resetInput } = useKeyPress();
+  const { prompt, input, lengths, fetchPrompt, resetInput, handleKeypress } = useKeyPress();
   const { codeLines, language } = prompt;
   const { wrongIndexes, counter } = input;
+  const { seconds, running, toggleTimer, resetTimer } = useTimer();
+
+  function newPrompt(language) {
+    fetchPrompt(language);
+    resetInput();
+    resetTimer();
+  }
 
   // side effect for language change
   useEffect(() => {
     newPrompt(language);
   }, []);
+
+  // timer toggling side effect for when user first presses a key
+  useEffect(() => {
+    if (!running && input.keys[0].length > 0) {
+      toggleTimer()
+    } else if (lengths[lengths.length - 1]) {
+      if (counter === lengths[lengths.length - 1][1] - 1)  {
+        toggleTimer()
+      }
+    }
+  }, [input]);
 
   // creates new event listener when component is unmounted (new prompt)
   useEffect(() => {
@@ -26,12 +45,12 @@ export default function Display() {
     };
   }, [handleKeypress]);
 
-
   return (
     <div className="display">
       <Information 
         language={prompt.language}
         setLanguage={newPrompt}
+        time={seconds}
       />
       <div className="codeContainer">
         <div className="line" />
