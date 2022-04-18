a4587c17504a06a1706d474e175bcdb9e3321f9a
Admin <adminsf@Admins-MacBook-Pro.local>
2022-04-17 19:00:13 -0700

    revamp user routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,25 +8,45 @@ const userSchema = require('../models/user');
 
 const topUsers = async (req, res) => {
   await userSchema.find({}).sort({ allTimeScore: -1 }).limit(10)
-    .then((prompt) => {
-      res.status(200).json(prompt);
+    .then((topUsers) => {
+      res.status(200).json(topUsers);
     })
     .catch((error) => {
       res.status(400).send(`Bad request. ${error}`);
     });
 };
 
-const addUser = async (req, res) => {
+const getUsers = async (req, res) => {
+  await userSchema.find({})
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      res.status(400).send(`Bad request. ${error}`);
+    });
+};
+
+const getUser = async (req, res) => {
+  await userSchema.find({ "email": req.params.email })
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      res.status(400).send(`Bad request. ${error}`);
+    });
+};
+
+const addUsers = async (req, res) => {
   await userSchema.insertMany(req.body)
-    .then((prompts) => {
-      res.status(200).json(prompts);
+    .then((users) => {
+      res.status(200).json(users);
     })
     .catch((error) => {
       res.status(400).send(`Not Saved there was an error. ${error}`);
     });
 };
 
-module.exports = { topUsers, addUser };
+module.exports = { topUsers, addUsers, getUsers, getUser };
 
 
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { topUsers, addUser } = require('../controllers/user');
+const { topUsers, addUsers, getUsers, getUser } = require('../controllers/user');
 
-router.get("/topUsers", topUsers);
-router.post("/user/add", addUser);
+router.get("/topUsers", topUsers); //Leaderboard
+router.get("/users", getUsers); //List of all users
+router.get("/user/:email", getUser); //Need a specific user (Maybe needs to be changed later)
+router.post("/users/add", addUsers); //Add users
 
 module.exports = router;
