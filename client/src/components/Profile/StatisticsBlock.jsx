ba27d2e62fb6bd97c217b1b68fe8f1a7ffff87a4
trijaychan <trijaychan@gmail.com>
2022-04-19 14:11:33 -0700

    Made profile page more consistent with other views

--- client/src/components/Profile/StatisticsBlock.jsx
+++ client/src/components/Profile/StatisticsBlock.jsx
@@ -2,33 +2,29 @@ import React from 'react';
 import './StatisticsBlock.scss';
 
 export default function StatisticsBlock(props) {
-
   const {timeSpent, numOfLessons, topScore, avgSpeed} = props;
 
   return (
-    <>
-      <p className="statistics-block">
-        <span className="frame">
-          <span className="inner top" title="Time spent on exercises">Total Time: </span>
-          <span className="inner bottom">{timeSpent}</span>
-        </span>
+    <p className="statistics-block">
+      <span className="frame">
+        <span className="inner top" title="Time spent on exercises">Total Time: </span>
+        <span className="inner bottom">{timeSpent}</span>
+      </span>
 
-        <span className="frame">
-          <span className="inner top" title="Number of lessons completed">Total Lessons: </span>
-          <span className="inner bottom">{numOfLessons}</span>
-        </span>
+      <span className="frame">
+        <span className="inner top" title="Number of lessons completed">Total Lessons: </span>
+        <span className="inner bottom">{numOfLessons}</span>
+      </span>
 
-        <span className="frame">
-          <span className="inner top" title="Top score">Top Score</span>
-          <span className="inner bottom">{topScore}</span>
-        </span>
+      <span className="frame">
+        <span className="inner top" title="Top score">Top Score</span>
+        <span className="inner bottom">{topScore}</span>
+      </span>
 
-        <span className="frame">
-          <span className="inner top" title="Average typing speed">Average Speed (wpm)</span>
-          <span className="inner bottom">{avgSpeed}</span>
-        </span>
-      </p>
-    </>
-  )
-
-}
+      <span className="frame">
+        <span className="inner top" title="Average typing speed">Average Speed (wpm)</span>
+        <span className="inner bottom">{avgSpeed}</span>
+      </span>
+    </p>
+  );
+};
