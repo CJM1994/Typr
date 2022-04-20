3b3df6fcff75bb6ada512247e45fee463dff0a59
CJM1994 <connorjohnmullin@gmail.com>
2022-04-19 20:15:25 -0700

    refactor useSocket and repurposing display code for multiplayer

--- client/src/components/Multiplayer/Multiplayer.jsx
+++ client/src/components/Multiplayer/Multiplayer.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
 import './Multiplayer.scss'
-const {io} = require('socket.io-client');
+import useSocket from '../../hooks/useSocket';
+import { useEffect, useState } from "react";
+import classNames from "classnames";
+import axios from "axios";
+import useKeyPress from "../../hooks/useKeyPress";
+import useTimer from "../../hooks/useTimer";
+import Lines from "../Main/Lines";
 
 export default function Multiplayer() {
 
-  io();
+  const { prompt, input, lengths, fetchPrompt, resetInput, handleKeypress, setFocus, endInput } = useKeyPress();
+  const { time, running, toggleTimer, resetTimer } = useTimer();
+  const { codeLines, language } = prompt;
+  const { wrongIndexes, counter } = input;
 
-  return(
-    <div>client</div>
+  const [stats, setStats] = useState({
+    wordsPerMin: 0,
+    accuracy: 0,
+    score: 0
+  });
+
+  function newPrompt(language) {
+    fetchPrompt(language); // sends get request for random prompt of a given language
+    resetInput(); // returns input to original state (wrongindexes, counter, keys, etc)
+    resetTimer(); // reset timer
+    setStats({
+      wordsPerMin: 0,
+      accuracy: 0,
+      score: 0
+    });
+  }
+
+  useEffect(() => { 
+    newPrompt('Javascript'); // Allows me to display a prompt with javascript
+                             // Will want to make a similar function that takes prompt from websocket
+  }, [])
+
+  useSocket();
+
+  return (
+        <div>
+          <Lines
+            lines={codeLines}
+            lengths={lengths}
+            indexes={wrongIndexes}
+            counter={counter}
+          />
+        </div>
   )
 
-}
+};
