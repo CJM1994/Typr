b5f91ca6f97397f8d6906c9ac90e6d27a6cd0326
CJM1994 <connorjohnmullin@gmail.com>
2022-04-26 11:38:59 -0700

    Add help text

--- client/src/components/Help/Help.js
+++ client/src/components/Help/Help.js
@@ -8,9 +8,16 @@ export default function Help() {
       <article className="body">
         <h1>Help</h1>
         <p className="text">
-          Typr aims to improve overall development through good programming practices such as...
+          <h2>Learn to type faster Touch typing</h2>
+          <br />
+          This web application will help you to learn touch typing, which means
+          typing through muscle memory without using your eyesight to find the
+          keys on the keyboard. It can improve your typing speed and accuracy
+          dramatically. The opposite is hunt-and-peck typing, a method of typing
+          in which you look at the keyboard instead of the screen, and use only
+          the index fingers.
         </p>
       </article>
     </section>
   );
-};
+}
