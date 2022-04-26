349ba98a4a044d4253c869180324db6cfad7c3ac
trijaychan <trijaychan@gmail.com>
2022-04-26 11:47:23 -0700

    Styled game over view

--- client/src/components/Multiplayer/Gameover.jsx
+++ client/src/components/Multiplayer/Gameover.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-import './Player.scss';
+import './Gameover.scss';
 
 export default function Gameover(props) {
   return (
     <div className='gameover-card'>
-      <h1>Game Over</h1>
+      <h1>Game Over!</h1>
       <h2>Winner: {props.winner}</h2>
       <span>
         <button type='button' onClick={()=> props.setWait('lobby')}>Return to lobby</button>
